b7e032b4bccb877b03a0f15457f61565ccb1c094
SamuelEbitNego <113426139+SamuelEbitNego@users.noreply.github.com>
2025-01-22 21:22:10 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ import note_app_lvl1Image from './foto/note_app_lvl1.JPG';
 import jssImage from './foto/jss.JPG';
 import backendImage from './foto/backend.JPG';
 import reactImage from './foto/react.JPG';
-import naposoImage from './foto/naposo.JPG;
+import naposoImage from './foto/naposoJPG';
 
 const projects = [
   { id: 1, title: 'QR Code Generate', description: 'This program Text To QR Code With Generate', foto: qrcodeImage, sourceCode: 'https://github.com/SamuelEbitNego/qrcodegenerate.git', source: 'https://qrcodegenerate-three.vercel.app/' },
